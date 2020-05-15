37eebe16db0599ec2e348e88f8512dfdb7637feb
Nick <laskarisn@gmail.com>
2020-05-15 14:00:23 +0300

    Added the Idp cache and populated the InfinispanCacheIdpProvider
    Need only to fill in the logic within the IdpCacheManager and
    IdpCacheSession

--- model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/InfinispanCacheIdpProviderFactory.java
+++ model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/InfinispanCacheIdpProviderFactory.java
@@ -1,35 +1,75 @@
 package org.keycloak.models.cache.infinispan;
 
+import org.infinispan.Cache;
+import org.jboss.logging.Logger;
 import org.keycloak.Config.Scope;
+import org.keycloak.cluster.ClusterEvent;
+import org.keycloak.cluster.ClusterProvider;
+import org.keycloak.connections.infinispan.InfinispanConnectionProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.models.cache.CacheIdpProvider;
 import org.keycloak.models.cache.CacheIdpProviderFactory;
-import org.keycloak.models.cache.CacheRealmProviderFactory;
+import org.keycloak.models.cache.infinispan.entities.Revisioned;
+import org.keycloak.models.cache.infinispan.events.InvalidationEvent;
 
 public class InfinispanCacheIdpProviderFactory implements CacheIdpProviderFactory {
 
+    private static final Logger log = Logger.getLogger(InfinispanCacheIdpProviderFactory.class);
+    
+    public static final String IDP_CLEAR_CACHE_EVENTS = "IDP_CLEAR_CACHE_EVENTS";
+    public static final String IDP_INVALIDATION_EVENTS = "IDP_INVALIDATION_EVENTS";
+	
+	protected volatile IdpCacheManager idpCache;
+	
+	
 	@Override
 	public CacheIdpProvider create(KeycloakSession session) {
-		// TODO Auto-generated method stub
-		return null;
+		lazyInit(session);
+        return new IdpCacheSession(idpCache, session);
 	}
 
+	private void lazyInit(KeycloakSession session) {
+        if (idpCache == null) {
+            synchronized (this) {
+                if (idpCache == null) {
+                    Cache<String, Revisioned> cache = session.getProvider(InfinispanConnectionProvider.class).getCache(InfinispanConnectionProvider.IDP_CACHE_NAME);
+                    Cache<String, Long> revisions = session.getProvider(InfinispanConnectionProvider.class).getCache(InfinispanConnectionProvider.IDP_REVISIONS_CACHE_NAME);
+                    idpCache = new IdpCacheManager(cache, revisions);
+
+                    ClusterProvider cluster = session.getProvider(ClusterProvider.class);
+
+                    cluster.registerListener(IDP_INVALIDATION_EVENTS, (ClusterEvent event) -> {
+
+                        InvalidationEvent invalidationEvent = (InvalidationEvent) event;
+                        idpCache.invalidationEventReceived(invalidationEvent);
+
+                    });
+
+                    cluster.registerListener(IDP_CLEAR_CACHE_EVENTS, (ClusterEvent event) -> {
+
+                        idpCache.clear();
+
+                    });
+
+                    log.debug("Registered cluster listeners for IdentityProviders Cache");
+                }
+            }
+        }
+    }
+	
 	@Override
 	public void init(Scope config) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void postInit(KeycloakSessionFactory factory) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
 		
 	}
 

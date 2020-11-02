2b8622da94f94ca1348d866580fc9065a3dde33c
laskarisn <laskarisn@admin.grnet.gr>
2020-11-02 17:00:39 +0200

    Halfway trustchains

--- plugins/keycloak-oidc-federation/src/main/java/org/keycloak/protocol/oidc/federation/processes/TrustChainProcessor.java
+++ plugins/keycloak-oidc-federation/src/main/java/org/keycloak/protocol/oidc/federation/processes/TrustChainProcessor.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.protocol.oidc.federation.beans.EntityStatement;
@@ -51,26 +54,68 @@ public class TrustChainProcessor {
 	/**
 	 * @throws BadSigningOrEncryptionException 
 	 * @throws UnparsableException 
-	 * This should construct all posible trust chains from a given leaf node url to a trust anchor url 
+	 * This should construct all possible trust chains from a given leaf node url to a trust anchor url 
 	 * @param leafNodeBaseUrl  this url should point to the base path of the leaf node (without the .well-known discovery subpath)
 	 * @param trustAnchorId this should hold the trust anchor id
 	 * @return any valid trust chains from the leaf node to the trust anchor.
 	 * @throws IOException 
 	 * @throws  
 	 */
-	public List<TrustChainRaw> constructTrustChains(String leafNodeBaseUrl, String trustAnchorId) throws IOException, UnparsableException, BadSigningOrEncryptionException {		
-		Set<String> visitedAuthHints = new HashSet<String>();
+	public List<TrustChainRaw> constructTrustChains(String leafNodeBaseUrl, Set<String> trustAnchorIds) throws IOException, UnparsableException, BadSigningOrEncryptionException {		
 		//TODO: create all possible links from the leaf node to the trust anchor
+		
+		trustAnchorIds.stream().map(s -> s.trim()).collect(Collectors.toCollection(HashSet::new));
+		
 		String encodedLeafES = Remote.getContentFrom(new URL(leafNodeBaseUrl + "/.well-known/openid-federation"));
 		
-		validateLinkSig(encodedLeafES);
-		EntityStatement leafES = parseChainLink(encodedLeafES);
-//		leafES.getAuthorityHints()
-//		visitedAuthHints.add(leafES.get)
+		if(!validateLinkSig(encodedLeafES))
+			return new ArrayList<TrustChainRaw>();
+		
+		TrustChainRaw trustChainRaw = new TrustChainRaw();
+		trustChainRaw.add(encodedLeafES);
+		fetchSubChain(trustChainRaw, trustAnchorIds);
+		
+		
+		//first find all possible paths
 		
 		return new ArrayList<TrustChainRaw>();
 	}
 	
+	private void fetchSubChain(TrustChainRaw trustChainRaw, Set<String> trustAnchorIds) {
+
+		String encodedES = trustChainRaw.get(trustChainRaw.size()-1);
+		EntityStatement es;
+		try {
+			es = parseChainLink(encodedES);
+		} catch (UnparsableException e) {
+			System.out.println("Cannot process a subchain link. Might not be able to form a trustchain. " + e.getMessage());
+			return;
+		}
+		if(es.getAuthorityHints() == null || es.getAuthorityHints().isEmpty()) {
+			if(es.getAuthorityHints().stream().anyMatch(authHint -> trustAnchorIds.contains(authHint)))
+				materializeChain(trustChainRaw);
+		}
+		else {
+			es.getAuthorityHints().forEach(authHint -> {
+				try {
+					String encodedSubES = Remote.getContentFrom(new URL(authHint + "/.well-known/openid-federation"));
+					if(validateLinkSig(encodedES)) {
+						TrustChainRaw subChainRaw = new TrustChainRaw();
+						subChainRaw.addAll(trustChainRaw);
+						subChainRaw.add(encodedSubES);
+						fetchSubChain(subChainRaw, trustAnchorIds);
+					}
+				}
+				catch(Exception ex) {
+					System.out.println("Cannot process a subchain. Might not be able to form a trustchain. " + ex.getMessage());
+				}
+			});
+		}
+	}
+	
+	private void materializeChain(TrustChainRaw trustChainRaw) {
+		
+	}
 	
 	/**
 	 * This validates the whole trustChain signature

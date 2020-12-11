8c30ca5d9a42d9dc80fecc54b8cfb6afe6654776
Konstantinos Georgilakis <cgeorgilakis@grnet.gr>
2020-12-11 16:44:05 +0200

    ClientExpiryTasks clear unused imports

--- plugins/keycloak-oidc-federation/src/main/java/org/keycloak/protocol/oidc/federation/tasks/ClientExpiryTasks.java
+++ plugins/keycloak-oidc-federation/src/main/java/org/keycloak/protocol/oidc/federation/tasks/ClientExpiryTasks.java
@@ -1,22 +1,19 @@
 package org.keycloak.protocol.oidc.federation.tasks;
 
-import java.time.LocalDateTime;
-
 import org.jboss.logging.Logger;
 import org.keycloak.common.util.Time;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.protocol.oidc.federation.helpers.FedUtils;
-import org.keycloak.protocol.oidc.federation.rest.OIDCFederationResourceProviderFactory;
 import org.keycloak.protocol.oidc.federation.schedule.DeleteExpiredClientScheduledTaskRunner;
 import org.keycloak.protocol.oidc.federation.schedule.OIDCFedScheduledTaskRunner;
 import org.keycloak.timer.TimerProvider;
 
 public class ClientExpiryTasks implements ClientExpiryTasksI {
-    
+
     private static final Logger logger = Logger.getLogger(ClientExpiryTasks.class);
 
-    private KeycloakSession session;    
-    
+    private KeycloakSession session;
+
     protected ClientExpiryTasks(KeycloakSession session) {
         this.session = session;
     }
@@ -27,20 +24,20 @@ public class ClientExpiryTasks implements ClientExpiryTasksI {
         TimerProvider timer = session.getProvider(TimerProvider.class);
         long interval =expiresAt - Long.valueOf(Time.currentTime())  ;
         //if client has expired already delete it now
-        if (interval <=0)
+        if (interval <=0) 
             interval =1;
         logger.info("Client with id= " + id + " will be deleted at " + expiresAt);
         String taskName = FedUtils.CLIENT_TASK_NAME + id + "." + realmId;
         timer.schedule(new OIDCFedScheduledTaskRunner(session.getKeycloakSessionFactory(),
             new DeleteExpiredClientScheduledTaskRunner(id, realmId), taskName), interval, taskName);
 
-    }   
-    
+    }
+
     @Override
     public void close() {
-        
+
     }
 
 
-    
+
 }

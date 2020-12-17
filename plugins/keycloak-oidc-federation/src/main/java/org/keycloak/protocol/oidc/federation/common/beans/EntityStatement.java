55f84df86e78bdc2091d3e914e7f0f7770aa29eb
laskarisn <laskarisn@admin.grnet.gr>
2020-12-17 12:20:56 +0200

    Adds {constraints, crit, policy_language_crit, trust_marks} fields on entity statement

--- plugins/keycloak-oidc-federation/src/main/java/org/keycloak/protocol/oidc/federation/common/beans/EntityStatement.java
+++ plugins/keycloak-oidc-federation/src/main/java/org/keycloak/protocol/oidc/federation/common/beans/EntityStatement.java
@@ -1,12 +1,18 @@
 package org.keycloak.protocol.oidc.federation.common.beans;
 
 import java.util.List;
+import java.util.Map;
 
 import org.keycloak.TokenCategory;
 import org.keycloak.jose.jwk.JSONWebKeySet;
+import org.keycloak.protocol.oidc.federation.common.beans.constraints.Constraints;
 import org.keycloak.representations.JsonWebToken;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 public class EntityStatement extends JsonWebToken {
 
@@ -19,6 +25,21 @@ public class EntityStatement extends JsonWebToken {
     
     @JsonProperty("metadata_policy")
     protected MetadataPolicy metadataPolicy;
+    
+    @JsonInclude(Include.NON_EMPTY)
+    protected Constraints constraints;
+    
+    @JsonInclude(Include.NON_EMPTY)
+    @JsonProperty("crit")
+    protected List<String> critical;
+    
+    @JsonInclude(Include.NON_EMPTY)
+    @JsonProperty("policy_language_crit")
+    protected List<String> policyLanguageCritical;
+    
+    @JsonInclude(Include.NON_EMPTY)
+    @JsonProperty("trust_marks")
+    protected List<TrustMark> trustMarks;
     
     
 	public List<String> getAuthorityHints() {
@@ -52,6 +73,38 @@ public class EntityStatement extends JsonWebToken {
     public void setMetadataPolicy(MetadataPolicy metadataPolicy) {
         this.metadataPolicy = metadataPolicy;
     }
+    
+    public Constraints getConstraints() {
+        return constraints;
+    }
+
+    public void setConstraints(Constraints constraints) {
+        this.constraints = constraints;
+    }
+
+    public List<String> getCritical() {
+        return critical;
+    }
+
+    public void setCritical(List<String> critical) {
+        this.critical = critical;
+    }
+
+    public List<String> getPolicyLanguageCritical() {
+        return policyLanguageCritical;
+    }
+
+    public void setPolicyLanguageCritical(List<String> policyLanguageCritical) {
+        this.policyLanguageCritical = policyLanguageCritical;
+    }
+
+    public List<TrustMark> getTrustMarks() {
+        return trustMarks;
+    }
+
+    public void setTrustMarks(List<TrustMark> trustMarks) {
+        this.trustMarks = trustMarks;
+    }
 
     @Override
     public TokenCategory getCategory() {
@@ -59,4 +112,14 @@ public class EntityStatement extends JsonWebToken {
     }
     
 	
+    @JsonAnyGetter
+    public Map<String, Object> getOtherClaims() {
+        return otherClaims;
+    }
+
+    @JsonAnySetter
+    public void setOtherClaims(String name, Object value) {
+        otherClaims.put(name, value);
+    }
+    
 }

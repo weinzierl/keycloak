4dc632009359dcd7331613a7820214b546255328
Konstantinos Georgilakis <cgeorgilakis@grnet.gr>
2022-03-15 17:00:19 +0200

    RCIAM-766 specific error page for no principals

--- server-spi-private/src/main/java/org/keycloak/events/Errors.java
+++ server-spi-private/src/main/java/org/keycloak/events/Errors.java
@@ -49,6 +49,7 @@ public interface Errors {
     String INVALID_TOKEN = "invalid_token";
     String INVALID_TOKEN_TYPE = "invalid_token_type";
     String INVALID_SAML_RESPONSE = "invalid_saml_response";
+    String NO_PRINCIPALS_FOUND = "no_principal_found";
     String INVALID_SAML_AUTHN_REQUEST = "invalid_authn_request";
     String INVALID_SAML_LOGOUT_REQUEST = "invalid_logout_request";
     String INVALID_SAML_LOGOUT_RESPONSE = "invalid_logout_response";
--- themes/src/main/resources/theme/base/login/error.ftl
+++ themes/src/main/resources/theme/base/login/error.ftl
@@ -4,7 +4,7 @@
         ${msg("errorTitle")}
     <#elseif section = "form">
         <div id="kc-error-message">
-            <p class="instruction">${message.summary?no_esc}</p>
+            <div class="instruction">${message.summary?no_esc}</div>
             <#if client?? && client.baseUrl?has_content>
                 <p><a id="backToApplication" href="${client.baseUrl}">${kcSanitize(msg("backToApplication"))?no_esc}</a></p>
             </#if>

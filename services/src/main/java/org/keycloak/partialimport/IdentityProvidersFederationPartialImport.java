9b3022505cc6f05bc1ae122a1b4f18e2f3fa32a2
Konstantinos Georgilakis <cgeorgilakis@grnet.gr>
2022-06-03 13:12:10 +0300

    Partial Import corrections and tests

--- services/src/main/java/org/keycloak/partialimport/IdentityProvidersFederationPartialImport.java
+++ services/src/main/java/org/keycloak/partialimport/IdentityProvidersFederationPartialImport.java
@@ -1,12 +1,15 @@
 package org.keycloak.partialimport;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.keycloak.broker.federation.FederationProvider;
 import org.keycloak.broker.federation.SAMLFederationProviderFactory;
+import org.keycloak.models.FederationMapperModel;
 import org.keycloak.models.FederationModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
+import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.models.utils.RepresentationToModel;
 import org.keycloak.representations.idm.SAMLFederationRepresentation;
 import org.keycloak.representations.idm.PartialImportRepresentation;
@@ -19,42 +22,47 @@ public class IdentityProvidersFederationPartialImport extends AbstractPartialImp
     }
 
     @Override
-    public String getName(SAMLFederationRepresentation metadataAggregate) {
-        return metadataAggregate.getAlias();
+    public String getName(SAMLFederationRepresentation rep) {
+        return rep.getAlias();
     }
 
     @Override
-    public String getModelId(RealmModel realm, KeycloakSession session, SAMLFederationRepresentation metadataAggregate) {
-        return metadataAggregate.getInternalId();
+    public String getModelId(RealmModel realm, KeycloakSession session, SAMLFederationRepresentation rep) {
+        return rep.getInternalId();
     }
 
     @Override
-    public boolean exists(RealmModel realm, KeycloakSession session, SAMLFederationRepresentation metadataAggregate) {
-        return realm.getSAMLFederationById(metadataAggregate.getInternalId()) != null;
+    public boolean exists(RealmModel realm, KeycloakSession session, SAMLFederationRepresentation rep) {
+        return realm.getSAMLFederationById(rep.getInternalId()) != null;
     }
 
     @Override
     public String existsMessage(RealmModel realm, SAMLFederationRepresentation metadataAggregate) {
-        return "Metadata Aggregate'" + getName(metadataAggregate) + "' already exists.";
+        return "SAML Federation'" + getName(metadataAggregate) + "' already exists.";
     }
 
     @Override
     public ResourceType getResourceType() {
-        return ResourceType.METADATA_AGGREGATE;
+        return ResourceType.SAML_FEDERATION;
     }
 
     @Override
-    public void remove(RealmModel realm, KeycloakSession session, SAMLFederationRepresentation metadataAggregate) {
-    	FederationModel model = RepresentationToModel.toModel(metadataAggregate);
-    	List<String> idps = realm.getIdentityProvidersByFederation(model.getInternalId());
-        idps.stream().forEach(idpAlias -> realm.removeFederationIdp(model, idpAlias));
-    	realm.removeSAMLFederation(model.getInternalId());
+    public void remove(RealmModel realm, KeycloakSession session, SAMLFederationRepresentation rep) {
+    	FederationModel model = RepresentationToModel.toModel(rep);
+        FederationProvider federationProvider = SAMLFederationProviderFactory.getSAMLFederationProviderFactoryById(session, model.getProviderId()).create(session,model,realm.getId());
+        federationProvider.removeFederation();
     }
 
     @Override
-    public void create(RealmModel realm, KeycloakSession session, SAMLFederationRepresentation metadataAggregate) {
-    	FederationModel model = RepresentationToModel.toModel(metadataAggregate);
+    public void create(RealmModel realm, KeycloakSession session, SAMLFederationRepresentation rep) {
+        rep.setInternalId(KeycloakModelUtils.generateId());
+        FederationModel model = RepresentationToModel.toModel(rep);
         realm.addSAMLFederation(model);
+        rep.getFederationMappers().stream().forEach(mapper -> {
+            mapper.setFederationId(rep.getInternalId());
+            FederationMapperModel mapperModel = RepresentationToModel.toModel(mapper);
+            realm.addIdentityProvidersFederationMapper(mapperModel);
+        });
         FederationProvider federationProvider = SAMLFederationProviderFactory.getSAMLFederationProviderFactoryById(session, model.getProviderId()).create(session,model,realm.getId());
         federationProvider.enableUpdateTask();
     }

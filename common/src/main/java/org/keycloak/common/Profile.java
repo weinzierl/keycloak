a296c2736d9545c6144a6899eebd61b430dba47a
Konstantinos Georgilakis <cgeorgilakis@grnet.gr>
2022-07-15 09:46:59 +0300

    test

--- common/src/main/java/org/keycloak/common/Profile.java
+++ common/src/main/java/org/keycloak/common/Profile.java
@@ -160,7 +160,7 @@ public class Profile {
         MAP_STORAGE("New store", Type.EXPERIMENTAL),
         PAR("OAuth 2.0 Pushed Authorization Requests (PAR)", Type.DEFAULT),
         DECLARATIVE_USER_PROFILE("Configure user profiles using a declarative style", Type.PREVIEW),
-        DYNAMIC_SCOPES("Dynamic OAuth 2.0 scopes", Type.EXPERIMENTAL),
+        DYNAMIC_SCOPES("Dynamic OAuth 2.0 scopes", Type.DEFAULT),
         CLIENT_SECRET_ROTATION("Client Secret Rotation", Type.PREVIEW),
         STEP_UP_AUTHENTICATION("Step-up Authentication", Type.DEFAULT),
         RECOVERY_CODES("Recovery codes", Type.PREVIEW);
